69561810a3a2beb516c7229863e7363b365ab5ab
Tyler Stegman <stegman44@gmail.com>
2020-03-24 15:22:32 -0400

    init commit

---
+++ ch02_StudentRegistrationProject/src/ch02_StudentRegistrationProject/StudentRegistrationApp.java
@@ -0,0 +1,28 @@
+package ch02_StudentRegistrationProject;
+
+import java.util.Scanner;
+
+public class StudentRegistrationApp {
+
+	public static void main(String[] args) {
+		System.out.println("Student Registration Form");
+		System.out.println();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter first name: ");
+		String firstName = sc.next();
+		System.out.println("Enter last name: ");
+		String lastName = sc.next();
+		System.out.println("Enter date of birth: ");
+		String birthDate = sc.next();
+		System.out.println();
+		System.out.print("Welcome "+firstName+" "+lastName+"!");
+		System.out.println();
+		System.out.println("Your registration is complete.");
+		System.out.println("Your temporary password is: "+firstName+"*"+birthDate);
+		}
+}	
+
+
+
---
+++ ch02_prj1_student_registration/src/StudentRegistrationApp.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class StudentRegistrationApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Student Registration App!");
+		
+		Scanner sc = new Scanner(System.in);
+		// get user input
+		System.out.print("Enter first name: ");
+		String firstName = sc.nextLine();
+		System.out.print("Enter last name: ");
+		String lastName = sc.nextLine();
+		System.out.print("Enter birth year: ");
+		int birthYear = sc.nextInt();
+		
+		// do business logic
+		String fullName = firstName + " " + lastName;
+		String tempPwd = firstName + "*" + birthYear;
+		
+		System.out.println();
+		
+		// display output
+		
+		System.out.println("Welcome "+fullName+"!");
+		System.out.println("Your registration is complete.");
+		System.out.println("Your temporary password is: "+tempPwd);
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("Bye!");
+		sc.close();
+		
+		
+
+	}
+
+}
