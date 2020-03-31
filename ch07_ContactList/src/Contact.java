d439f87dffc0a4ca19eeea2267514134e9ef77c9
Tyler Stegman <stegman44@gmail.com>
2020-03-31 10:30:18 -0400

    Contact List final

--- ch07_ContactList/src/Contact.java
+++ ch07_ContactList/src/Contact.java
@@ -1,22 +1,72 @@
 
 public class Contact {
-	
+	// 1. define instance variables 
+	// firstName, lastName, email, phone - in this example
 	private String firstName;
 	private String lastName;
 	private String email;
-	private String phoneNumber;
-	private String displayContact;
+	private String phone;
 	
-	public Contact(String firstName, String lastName, String email, String phoneNumber, String displayContact) {
+	// 2. 'generate' constructors - use IDE *always 2 constructors: default(empty) and fully loaded
+	public Contact() {
+		super();
+	}
+	
+	public Contact(String firstName, String lastName, String email, String phoneNumber) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
-		this.phoneNumber = phoneNumber;
-		this.displayContact = displayContact;
+		this.phone = phoneNumber;
+	}
+
+	// 3. 'generate' getters/setters - use IDE
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 	
-	
-	
+	// define displayContact method - this example
+	public String displayContact() {
+		String contactStr = "-------------------------------\n" + 
+							"---- Current Contact ----------\n" +
+							"-------------------------------\n" +
+							"Name:          " + firstName+" "+lastName+"\n" +
+							"Email Address: "+email+"\n" +
+							"Phone Number:  "+phone+"\n" +
+							"-------------------------------\n";
+		return contactStr;
+	}
 
+	
+	
+	
+	// 4. [optional] 'generate' toString()
 }
--- ch07_ContactList/src/ContactListApp.java
+++ ch07_ContactList/src/ContactListApp.java
@@ -3,45 +3,26 @@ import java.util.Scanner;
 public class ContactListApp {
 
 	public static void main(String[] args) {
+		// print welcome message
 		System.out.println("Welcome to the Contact List application");
 		System.out.println();
 		
-		Scanner sc = new Scanner(System.in);
-		String choice = "y";
-		while (choice.equalsIgnoreCase("y")) {
-			System.out.print("Enter first name: ");
-			String firstName = sc.next();
-			sc.nextLine();
-			System.out.print("Enter last name: ");
-			String lastName = sc.next();
-			sc.nextLine();
-			System.out.print("Enter email: ");
-			String email = sc.next();
-			sc.nextLine();
-			System.out.print("Enter phone: ");
-			String phoneNumber = sc.next();
-			sc.nextLine();
-			System.out.println();
-			System.out.println("--------------------------------------------");
-			System.out.println("---- Current Contact -----------------------");
-			System.out.println("--------------------------------------------");
-			System.out.println("Name: "+(firstName +" "+ lastName));
-			System.out.println("Email Address: "+email);
-			System.out.println("Phone Number: "+phoneNumber);
-			break;
+		// NOTE: skipping continue loop to save time
+		// get user input: String firstName, lastName, email, phoneNumber
+		String fn = Console.getString("Enter first name:  ");
+		String ln = Console.getString("Enter last name:  ");
+		String e = Console.getString("Enter email:  ");
+		String pn = Console.getString("Enter phone:  ");
+		
+		// Do biz logic??  Create an instance of Contact
+		Contact c = new Contact(fn,ln,e,pn);
 			
-		}
-			
-		
-		
-		
-		
-		
-		
-		
-		
+		// display results: Current contact display - per specs
+		System.out.println(c.displayContact());
+
+		// goodbye
 		System.out.println("Bye!");
-		sc.close();
+		
 	}
 
 }
