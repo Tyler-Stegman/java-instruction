e3ec6b60f55738eb0febefcbfc863b342fe692b0
Tyler Stegman <stegman44@gmail.com>
2020-03-31 16:15:30 -0400

    Abstracts Instruction

---
+++ ch08_AbstractCar/src/AbstractCarsApp.java
@@ -0,0 +1,10 @@
+
+public class AbstractCarsApp {
+
+	public static void main(String[] args) {
+		SportUtilityVehicle suv1 = new SportUtilityVehicle("Honda", "CRV", 2019, 23, false, false);
+		System.out.println(suv1);
+
+	}
+
+}
