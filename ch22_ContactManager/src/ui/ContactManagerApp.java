523d1db957b42fa14bbd64d0c4f4047e0c9989b7
Tyler Stegman <stegman44@gmail.com>
2020-04-13 16:54:48 -0400

    Ch. 22 Contact Manager Class Instruction

---
+++ ch22_ContactManager/src/ui/ContactManagerApp.java
@@ -0,0 +1,66 @@
+package ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import business.Contact;
+import business.TestContact;
+
+public class ContactManagerApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Contact Manager App!");
+
+		List<Contact> contacts = new ArrayList<>();
+
+		contacts.add(new Contact("Mike Murach", null, "800-221-5528"));
+		contacts.add(new Contact("Anne Boehm", null, null));
+		contacts.add(new Contact("Joel Murach", "joel@murach.com", null));
+
+		// print out a list of all contacts
+		System.out.println("All contacts:");
+		displayContacts(contacts);
+		System.out.println();
+		System.out.println("List of contacts missing phone #'s - no lamda:");
+		List<Contact> contactsNoPhone = filterContactsWithoutPhone(contacts);
+		displayContacts(contactsNoPhone);
+		System.out.println();
+		System.out.println("List of contacts missing phone #'s - lamda:");
+		List<Contact> contactsNoPhone2 = filterContacts(contacts, c -> c.getPhone() == null);
+		displayContacts(contactsNoPhone2);
+		System.out.println();
+		System.out.println("List of contacts missing email - lamda:");
+		List<Contact> contactsNoEmail = filterContacts(contacts, c -> c.getEmail() == null);
+		displayContacts(contactsNoEmail);
+		
+		System.out.println("Bye");
+
+	}
+
+	private static void displayContacts(List<Contact> contacts) {
+		for (Contact c : contacts) {
+			System.out.println(c);
+		}
+	}
+
+	public static List<Contact> filterContactsWithoutPhone(List<Contact> contacts) {
+		List<Contact> contactWithoutPhone = new ArrayList<>();
+		for (Contact c : contacts) {
+			if (c.getPhone() == null) {
+				contactWithoutPhone.add(c);
+			}
+		}
+		return contactWithoutPhone;
+	}
+	
+	public static List<Contact> filterContacts(List<Contact> contacts, TestContact condition) {
+		List<Contact> filteredContacts = new ArrayList<>();
+		for (Contact c : contacts) {
+			if(condition.test(c)) {
+				filteredContacts.add(c);
+			}
+		}
+		
+		return filteredContacts;
+	}
+}
