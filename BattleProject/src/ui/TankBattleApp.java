a5be5c174e26bf54954fd112160cfe09a198acd8
Tyler Stegman <stegman44@gmail.com>
2020-04-23 17:25:09 -0400

    Normandy '44

--- BattleProject/src/ui/TankBattleApp.java
+++ BattleProject/src/ui/TankBattleApp.java
@@ -22,20 +22,16 @@ public class TankBattleApp {
 		tanks.add(t4);
 		tanks.add(t5);
 		tanks.add(t6);
+		//tanks.get(5).getArmor();
+		//tanks.get(5).setArmor(tanks.get(5).getArmor()-100);
 
-		System.out.println("            Tank Battle!\n\n" + "   	 [ O ]\n" + "     	   \\ \\   \n"
+		System.out.println("            Normandy '44\n\n" + "   	 [ O ]\n" + "     	   \\ \\   \n"
 				+ "      	    \\ \\  \n" + "             \\ \\--'---_\n" + "       	   _/ \\ \\   // ~~\\_\n"
 				+ "   .//---//__|=//_//------/~~~\\\n" + "   //___________________/O   O \\\n"
 				+ "   (===(\\_________(===(Oo o o O)          \n" + "   \\~~~\\____//     \\---\\Oo__o--\n"
 				+ "    ~~~~~~~        ~~~~~~~~~~\n");
-
-		/*
-		 * System.out.println("  Tank Battle!"); System.out.println("      _____");
-		 * System.out.println("   __|_>o<_|__"); System.out.println("  /___________\\");
-		 * System.out.println("  \\=\\>-----</=/\n");
-		 */
-		String userName = Console.getString("Enter new username: ", true);
-		System.out.println("Welcome, " + userName + ", to the Tank Battle App!\n");
+		String userName = Console.getString("Enter new Commander name: ", true);
+		System.out.println(userName + ", welcome to Europe.  Let's get to the front!\n");
 		String command = "";
 		while (!command.equalsIgnoreCase("exit")) {
 			displayMenu();
@@ -66,7 +62,7 @@ public class TankBattleApp {
 			System.out.println(t);
 		}
 		int tankId = Console.getInt("Tank ID: ", 0, tanks.size() + 1);
-		System.out.println(tankId + " was selected.");
+		System.out.println(tankId + " was selected.\n");
 		if (tankId == 1) {
 			System.out.println("Panzerkampfwagen VI (Tiger I)\n");
 			System.out.println(
@@ -85,7 +81,7 @@ public class TankBattleApp {
 							+ " PzKpfw VI Ausf. E in March 1943, with ordnance inventory designation Sd.Kfz. 181.\n");
 		}
 		if (tankId == 2) {
-			System.out.println("Panzerkampfwagen V (Panther I)");
+			System.out.println("Panzerkampfwagen V (Panther I)\n");
 			System.out.println(
 					"The Panther is a German medium tank deployed during World War II on the Eastern and Western Fronts in\n"
 							+ " Europe from mid-1943 to the war's end in 1945.  The Panther was intended to counter the Soviet T-34\n"
@@ -98,7 +94,7 @@ public class TankBattleApp {
 							+ " explosive firepower against infantry.\n");
 		}
 		if (tankId == 3) {
-			System.out.println("Panzerkampfwagen IV (Panzer IV)");
+			System.out.println("Panzerkampfwagen IV (Panzer IV)\n");
 			System.out.println(
 					"The Panzer IV was the most numerous German tank and the second-most numerous German armored fighting vehicle of the\n"
 							+ " Second World War, with some 8,500 built. It received various upgrades and design modifications, intended to counter\n"
@@ -108,7 +104,7 @@ public class TankBattleApp {
 							+ " in all combat theaters involving Germany and was the only German tank to remain in continuous production throughout the war.\n");
 		}
 		if (tankId == 4) {
-			System.out.println("M26 Pershing");
+			System.out.println("M26 Pershing\n");
 			System.out.println(
 					"The M26 Pershing was a heavy tank/medium tank of the United States Army.  It was used in the last months of World War II during\n"
 							+ " the invasion of Germany.  Intended as a replacement of the M4 Sherman,[citation needed] a prolonged development period meant that\n"
@@ -117,7 +113,7 @@ public class TankBattleApp {
 							+ " behind the German Panther medium tank, but ahead of the Tiger I heavy tank.\n");
 		}
 		if (tankId == 5) {
-			System.out.println("M4 Sherman");
+			System.out.println("M4 Sherman\n");
 			System.out.println(
 					"The M4 Sherman, officially Medium Tank, M4, was the most widely used medium tank by the United States and Western Allies in World War II.\n"
 							+ " The M4 Sherman proved to be reliable, relatively cheap to produce, and available in great numbers. It was also the basis of several\n"
@@ -132,7 +128,7 @@ public class TankBattleApp {
 							+ " The M4 went on to be produced in large numbers, being the most produced tank in American history.\n");
 		}
 		if (tankId == 6) {
-			System.out.println("M3 Lee");
+			System.out.println("M3 Stuart\n");
 			System.out.println(
 					"The M3 Stuart, officially Light Tank, M3, was an American light tank of World War II.  It was supplied to British and other Commonwealth forces\n"
 							+ " under lend-lease prior to the entry of the U.S. into the war. Thereafter, it was used by U.S. and Allied forces until the end of the war.\n"
@@ -143,123 +139,110 @@ public class TankBattleApp {
 		}
 	}
 
-	// TODO select Tank and play game?
 	private static void play() {
 		selectTank();
-		String choice = "y";
-		System.out.println("COMMANDER: 'Driver stop!'\n" + "'Gunner heat tank!...'\n" + "GUNNER: 'Identified!'\n");
-		while (choice.equalsIgnoreCase("y") || choice.equalsIgnoreCase("Y")) {
+		String choice = Console.getString("Continue? (y/n): ", "y", "n");
+		while (choice.equalsIgnoreCase("y")) {
+			System.out.println(
+					"July 1944.  Barely a month after the Allied invasion of Normandy, the US First Army captured\n"
+							+ "Cherbourg, and began their attack south towards Saint-Lo.  After a weather delay, the First Army began\n"
+							+ "Operation Cobra on the Saint-Lo-Periers road.  With orders to enter fray at the front,\n"
+							+ "you and your crew join an armored column traveling the Saint-Lo-Periers Road.  Near the city,\n"
+							+ "your armored column is ambushed by a number of enemy tanks and artillery.  With the elements of the\n"
+							+ "front and rear of the column knocked out, your tank becomes trapped in a vortex of murderous fire.\n"
+							+ "In a desperate attempt to break the ambush, you move to engage enemy targets...\n");
+			System.out.println("COMMANDER: Driver stop! Gunner heat tank!...\n" + "GUNNER: Identified!\n");
+			break;
+		}
+		while (choice.equalsIgnoreCase("y")) {
+			System.out.println("LOADER: Up!\n");
+			Console.getString("COMMANDER: 'FIRE!' (y): ", true);
 			int roll1 = (int) (Math.random() * 6) + 1;
-			System.out.println("LOADER: 'Up!'");
-			Console.getString("COMMANDER: 'FIRE!' (y/n): ", true);
 			if ((int) roll1 == 1) {
-				System.out.println("GUNNER: 'On the way!'");
-				System.out.println("COMMANDER: 'Miss!'\n");
-				System.out.println("'Driver, Maneuver to flank position!'\n");
-				System.out.println("DRIVER: 'Yes, Sir!'");
+				System.out.println("GUNNER: On the way!\n" + "COMMANDER: Miss!\n"
+						+ "Driver, maneuver to flank posiiton!\n" + "DRIVER: Yes, Sir!\n");
 				enemyTank();
 				continue;
-			}
-			if ((int) roll1 == 2) {
-				System.out.println("GUNNER: 'On the way!'");
-				System.out.println("COMMANDER: 'Shot deflected!'\n");
-				System.out.println("'Driver, get this thing in position!'\n");
-				System.out.println("DRIVER: 'Maneuvering!'\n");
+			} else if ((int) roll1 == 2) {
+				System.out.println("GUNNER: On the way!\n" + "COMMANDER: Shot deflected!\n"
+						+ "Driver, get this thing in position!\n" + "DRIVER: Maneuvering!\n");
 				System.out.println("    ___\r\n" + " __(   )====::\r\n" + "/~~~~~~~~~\\\r\n" + "\\O.O.O.O.O/\n");
 				enemyTank();
 				continue;
-			}
-			if ((int) roll1 == 3) {
-				System.out.println("GUNNER: 'On the way!'");
-				System.out.println("COMMANDER: 'On target! Enemy is still engaging.  Get that gun loaded!'\n");
+			} else if ((int) roll1 == 3) {
+				System.out.println("GUNNER: On the way!\n"
+						+ "COMMANDER: On target! Enemy is still engaging.  Get that gun loaded!\n");
 				enemyTank();
 				continue;
-			}
-			if ((int) roll1 == 4) {
-				System.out.println("GUNNER: 'On the way!'");
-				System.out.println("COMMANDER: 'Hit! Enemy taking damage!  Stay on target!'\n");
+			} else if ((int) roll1 == 4) {
+				System.out.println("GUNNER: On the way!\n" + "COMMANDER: Hit! Enemy taking damage!  Stay on target!\n");
 				enemyTank();
 				continue;
-			}
-			if ((int) roll1 == 5) {
-				System.out.println("GUNNER: 'On the way!'");
-				System.out.println("COMMANDER: 'Direct hit! Enemy hull penetrated!  Pour it on 'em!'\n");
+			} else if ((int) roll1 == 5) {
+				System.out.println(
+						"GUNNER: On the way!\n" + "COMMANDER: Direct hit! Enemy hull penetrated!  Pour it on 'em!\n");
 				enemyTank();
 				continue;
+			} else if ((int) roll1 == 6) {
+				System.out.println("GUNNER: On the way!\n" + "COMMANDER: Target cease fire.\n"
+						+ "Good shooting! Enemy Tank destroyed!\n");
+				break;
 			}
-			if ((int) roll1 == 6) {
-				System.out.println("GUNNER: 'On the way!'");
-				System.out.println("COMMANDER: 'Target cease fire'");
-				System.out.println("'Good shooting! Enemy Tank destroyed!'\n");
-				play();
-			}
-			Console.getString("Continue? (y/n): ", true);
-			break;
 		}
+		// Console.getString("Continue? (y/n): ", "y", "n");
 	}
 
 	private static void selectTank() {
-		String choice = "y";
-		while (choice.equalsIgnoreCase("y") || choice.equalsIgnoreCase("Y")) {
-			System.out.println();
-			System.out.println("Available Tanks\n" + "---------------\n");
-			for (Tank t : tanks) {
-				System.out.println(t);
-			}
-			System.out.println("Select your tank!\n" + "----------------------\n");
-			int tankId = Console.getInt("Tank ID: ", 0, tanks.size() + 1);
-			if (tankId == 1) {
-				System.out.println("Your opponent: M26 Pershing\n");
-			} else if (tankId == 2) {
-				System.out.println("Your opponent: M4 Sherman\n");
-			} else if (tankId == 3) {
-				System.out.println("Your opponent: M3 Lee\n");
-			} else if (tankId == 4) {
-				System.out.println("Your opponent: Tiger I\n");
-			} else if (tankId == 5) {
-				System.out.println("Your opponent: Panther I\n");
-			} else if (tankId == 6) {
-				System.out.println("Your opponent: Panzer IV\n");
-			}
-			Console.getString("Deploy? (y/n): ", true);
-			break;
+		System.out.println();
+		System.out.println("Available Tanks\n" + "---------------\n");
+		for (Tank t : tanks) {
+			System.out.println(t);
+		}
+		System.out.println("Select your tank!\n" + "----------------------\n");
+		int tankId = Console.getInt("Tank ID: ", 0, tanks.size() + 1);
+		if (tankId == 1) {
+			System.out.println("Your opponent: M26 Pershing\n");
+		} else if (tankId == 2) {
+			System.out.println("Your opponent: M4 Sherman\n");
+		} else if (tankId == 3) {
+			System.out.println("Your opponent: M3 Stuart\n");
+		} else if (tankId == 4) {
+			System.out.println("Your opponent: Tiger I\n");
+		} else if (tankId == 5) {
+			System.out.println("Your opponent: Panther I\n");
+		} else if (tankId == 6) {
+			System.out.println("Your opponent: Panzer IV\n");
 		}
 	}
 
 	private static void enemyTank() {
-
-			int roll2 = (int) (Math.random() * 6) + 1;
-			if ((int) roll2 == 1) {
-				System.out.println("Enemy shot: Miss!\n");
-				System.out.println("COMMANDER: 'They're trying to flank!'\n");
-				System.out.println("      _____");
-				System.out.println("   __|_>o<_|__");
-				System.out.println("  /___________\\");
-				System.out.println("  \\=\\>-----</=/\n");
-			}
-			if ((int) roll2 == 2) {
-				System.out.println("Enemy shot: Enemy shot deflected!\n");
-			}
-			if ((int) roll2 == 3) {
-				System.out.println("Enemy shot: Minor damage.");
-				System.out.println("COMMANDER: 'We've taken a hit, sustaining!'\n");
-			}
-			if ((int) roll2 == 4) {
-				System.out.println("Enemy shot: Hit.");
-				System.out.println("DRIVER: 'Sir, our hydraulics are shot!'\n");
-			}
-			if ((int) roll2 == 5) {
-				System.out.println("Enemy shot: Direct hit! ");
-				System.out.println("DRIVER: 'Sir, we're taking heavy damage! We can't take much more of this!'\n");
-				System.out.println("COMMANDER: 'Steady, boys...'");
-			}
-			if ((int) roll2 == 6) {
-				System.out.println("Enemy shot: ");
-				System.out.println("COMMANDER: 'INCOMI---!'");
-				System.out.println("SYSTEM: Magazine hit, tank destroyed!\n");
-				System.out.println("Game Over\n");
-				play();
-			}
+		int roll2 = (int) (Math.random() * 6) + 1;
+		if ((int) roll2 == 1) {
+			System.out.println("Enemy shot: Miss!\n" + "COMMANDER: 'They're trying to flank!'\n");
+			System.out.println("      _____");
+			System.out.println("   __|_>o<_|__");
+			System.out.println("  /___________\\");
+			System.out.println("  \\=\\>-----</=/\n");
+		}
+		if ((int) roll2 == 2) {
+			System.out.println("Enemy shot: Enemy shot deflected!\n");
+		}
+		if ((int) roll2 == 3) {
+			System.out.println("Enemy shot: Minor damage.\n" + "COMMANDER: We've taken a hit, sustaining!\n");
+		}
+		if ((int) roll2 == 4) {
+			System.out.println("Enemy shot: Hit.\n" + "DRIVER: Sir, our hydraulics are shot!\n");
+		}
+		if ((int) roll2 == 5) {
+			System.out.println("Enemy shot: Direct hit!\n"
+					+ "DRIVER: Sir, we're taking heavy damage! We can't take much more of this!\n"
+					+ "COMMANDER: Steady, boys...\n");
+		}
+		if ((int) roll2 == 6) {
+			System.out.println("Enemy shot: \n" + "COMMANDER: INCOMI---!\n"
+					+ "SYSTEM: Magazine hit, your tank has been destroyed!\n");
+			endGame();
+		}
 	}
 
 	private static void displayMenu() {
@@ -269,4 +252,38 @@ public class TankBattleApp {
 		menu.append("exit      -     exit program\n");
 		System.out.println(menu);
 	}
+
+	private static void endGame() {
+		System.out.println("Game Over");
+		String end = Console.getString("Main Menu? (y/n) ", "y", "n");
+		if (end.equalsIgnoreCase("y")) {
+			mainMenu();
+		} else {
+			System.out.println("Thank you for playing!");
+			System.exit(0);
+
+		}
+	}
+
+	private static void mainMenu() {
+		String command = "";
+		while (!command.equalsIgnoreCase("exit")) {
+			displayMenu();
+			command = Console.getString("Command: ", true);
+			switch (command) {
+			case "history":
+				history();
+				break;
+			case "play":
+				play();
+			case "exit":
+				System.out.println("Thanks for playing!");
+				System.exit(0);
+			default:
+				System.out.println("Invalid Entry. Try again");
+				break;
+			}
+		}
+	}
+
 }
